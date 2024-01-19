cadb46abbdd95c81cceb0dee4fe818341ae203e5
PranayBisan45 <pranaybisan45@gmail.com>
2024-01-19 12:42:38 +0530

    Day14

--- Day15/Authorization/src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-insert into users(id, password, user_name) values(1, 'pass', 'admin');
-insert into users(id, password, user_name) values(2, 'pass', 'user');
--- Day16/productcrudaxios/src/Components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './Footer.css'
-export default function Header() {
-  return (
-    <div>
-        <h1 className="header">Product Management system</h1>
-    </div>
-  )
-}
